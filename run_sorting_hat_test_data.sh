2530a7ca47b278d0e795aec6b2a103b81a915ca0
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-07-29 16:41:56 +0100

    Conda prefix and better logpath handling

--- run_sorting_hat_test_data.sh
+++ run_sorting_hat_test_data.sh
@@ -4,14 +4,25 @@ GTF=$( pwd )/test-data/gff
 BIOENTITIES_PROPERTIES=$( pwd )/test-data/bioentity_properties
 SORTING_HAT=$( pwd )/Snakefile-sorting-hat
 LOG_HANDLER=$( pwd )/log_handler.py
+CONDA_PREFIX=${CONDA_PREFIX:-$( pwd )/conda_installs}
 
+CONDA_PREFIX_LINE="--conda-prefix $CONDA_PREFIX"
 export LOG_PATH=${LOG_PATH:-$( pwd )/sorting.log}
+USUAL_SM_ERR_OUT=${USUAL_SM_ERR_OUT:-$( pwd )/snakemake.log}
 pushd $1
+
+rm -f $LOG_PATH
+touch $LOG_PATH
+tail -f $LOG_PATH &
 
 snakemake --use-conda --conda-frontend mamba \
         --log-handler-script $LOG_HANDLER \
+        $CONDA_PREFIX_LINE \
         --keep-going \
-        --config gtf_dir=$GTF sm_options="--use-conda --conda-frontend mamba -j 2" \
-        bioentities_properties=$BIOENTITIES_PROPERTIES -j 1 -s $SORTING_HAT &>/dev/null
+        --config gtf_dir=$GTF \
+          sm_options="--use-conda --conda-frontend mamba --keep-going -j 2 $CONDA_PREFIX_LINE " \
+        bioentities_properties=$BIOENTITIES_PROPERTIES -j 1 -s $SORTING_HAT &> $USUAL_SM_ERR_OUT
 
-cat $LOG_PATH
+sleep 5
+# remove tail process
+kill $(jobs -p)
