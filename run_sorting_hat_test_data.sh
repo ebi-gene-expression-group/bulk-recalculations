e816fab476d4e2de52b510a5e7252e9558734407
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-09-07 16:28:18 +0100

    These fixes are now up-to-date re creation of conda environments in a dry run.

--- run_sorting_hat_test_data.sh
+++ run_sorting_hat_test_data.sh
@@ -10,23 +10,21 @@ SN_CONDA_PREFIX=${SN_CONDA_PREFIX:-$( pwd )/conda_installs}
 CONDA_PREFIX_LINE="--conda-prefix $SN_CONDA_PREFIX"
 export LOG_PATH=${LOG_PATH:-$( pwd )/sorting.log}
 USUAL_SM_ERR_OUT=${USUAL_SM_ERR_OUT:-$( pwd )/snakemake.log}
-pushd $1
 
+#create conda envs only
+for yaml_file in $(ls $( pwd )/envs); do
+  echo $yaml_file
+  snakemake --dry-run --use-conda --conda-create-envs-only $CONDA_PREFIX_LINE  \
+            --conda-frontend mamba \
+            --config \
+            yamlFile=$( pwd )/envs/$yaml_file \
+            -s Snakefile-create-conda-envs
+done
+
+pushd $1
 rm -f $LOG_PATH
 touch $LOG_PATH
 tail -f $LOG_PATH &
-
-snakemake --dryrun --quiet --delete-temp-output --use-conda --conda-create-envs-only --conda-frontend mamba \
-        $CONDA_PREFIX_LINE \
-        --keep-going \
-        --config gtf_dir=$GTF \
-        atlas_prod=path/to/atlasprod \
-        atlas_exps=path/to/atlasexps \
-        atlas_meta_config=path/to/supporting_files \
-          sm_options="--use-conda --conda-frontend mamba --keep-going -j 2 $CONDA_PREFIX_LINE " \
-        bioentities_properties=$BIOENTITIES_PROPERTIES -j 1 -s $SORTING_HAT &> ${USUAL_SM_ERR_OUT}.dryrun
-echo 'dry run completed - conda envs created.'
-echo 'starting bulk-recalculations...'
 
 snakemake --use-conda --conda-frontend mamba \
         --log-handler-script $LOG_HANDLER \
