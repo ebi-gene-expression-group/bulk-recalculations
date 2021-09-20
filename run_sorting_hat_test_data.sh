bc0ea05c50314b68668a81f987347b78890f966a
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-09-20 14:00:40 +0100

    Adds python script to check species names based on a YAML dictionary

--- run_sorting_hat_test_data.sh
+++ run_sorting_hat_test_data.sh
@@ -3,6 +3,8 @@
 GTF=$( pwd )/test-data/gff
 DELETE_PREV_OUTPUT=True
 SKIP_STEPS=$( pwd )/step_skip.yaml
+# CHECK_SPECIES file should come from a clone of the atlas-config repo in Jenkins:
+CHECK_SPECIES=$( pwd )/Bulk_recalculations_check_species_names.yaml
 BIOENTITIES_PROPERTIES=$( pwd )/test-data/bioentity_properties
 SORTING_HAT=$( pwd )/Snakefile-sorting-hat
 LOG_HANDLER=$( pwd )/log_handler.py
@@ -30,6 +32,7 @@ snakemake --use-conda --conda-frontend mamba \
         sm_options="--use-conda --conda-frontend mamba --keep-going -j 2 $CONDA_PREFIX_LINE " \
         delete_previous_output=$DELETE_PREV_OUTPUT \ 
         skip_steps_file=$SKIP_STEPS \  
+        check_sp_file=$CHECK_SPECIES \
         bioentities_properties=$BIOENTITIES_PROPERTIES -j 1 -s $SORTING_HAT &> $USUAL_SM_ERR_OUT
 
 sleep 5
