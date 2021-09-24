81ef4884deb8ff9b2cf81cfd5aed9826fcbc74a7
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-09-24 13:32:40 +0100

    Update run_sorting_hat_test_data.sh

--- run_sorting_hat_test_data.sh
+++ run_sorting_hat_test_data.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 GTF=$( pwd )/test-data/gff
-#FORCEALL=$true
+FORCEALL=$true
+RESTART_TIMES=1
 SKIP_STEPS=$( pwd )/step_skip.yaml
+CHECK_SPECIES=$( pwd )/Bulk_recalculations_check_species_names.yaml
 BIOENTITIES_PROPERTIES=$( pwd )/test-data/bioentity_properties
 SORTING_HAT=$( pwd )/Snakefile-sorting-hat
 LOG_HANDLER=$( pwd )/log_handler.py
 SN_CONDA_PREFIX=${SN_CONDA_PREFIX:-$( pwd )/conda_installs}
+if [ $FORCEALL ]
+then
+  PROFILE_LINE="--profile profilename --restart-times "$RESTART_TIMES
+else
+  PROFILE_LINE="--profile profilename --forceall --restart-times "$RESTART_TIMES
+fi
 
 CONDA_PREFIX_LINE="--conda-prefix $SN_CONDA_PREFIX"
 export LOG_PATH=${LOG_PATH:-$( pwd )/sorting.log}
@@ -31,14 +39,17 @@ echo 'starting bulk-recalculations...'
 
 snakemake --use-conda --conda-frontend mamba \
         --log-handler-script $LOG_HANDLER \
+        $PROFILE_LINE \
         $CONDA_PREFIX_LINE \
+        --latency-wait 10 \
         --keep-going \
         --config gtf_dir=$GTF \
         atlas_prod=path/to/atlasprod \
         atlas_exps=path/to/atlasexps \
         atlas_meta_config=path/to/supporting_files \
-        sm_options="--use-conda --conda-frontend mamba --keep-going -j 2 $CONDA_PREFIX_LINE " \
-        skip_steps_file=$SKIP_STEPS \  
+        skip_steps_file=$SKIP_STEPS \
+        check_sp_file=$CHECK_SPECIES \
+        sm_options="--use-conda --conda-frontend mamba --keep-going $PROFILE_LINE -j 2 $CONDA_PREFIX_LINE " \
         bioentities_properties=$BIOENTITIES_PROPERTIES -j 1 -s $SORTING_HAT &> $USUAL_SM_ERR_OUT
 
 sleep 5
