13c38e901b41fa4f45c6df4c68eb870e01698ab3
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-09-25 19:30:35 +0100

    Converts Python script to Perl in `rule get_experiment_metadata`

---
+++ bin/bulk_recalculations_check_species_names.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use YAML qw(LoadFile);
+
+my ($input_sp, $YAML_file) = @ARGV;
+
+# get total arg passed to this script
+my $total = $#ARGV + 1;
+print "Total args passed to the script: $total\n";
+
+if ($total != 2) {
+  die "The total arg passed is not equal to 2 (<organism> <yaml_file>)\n";
+}
+
+print "Searching '$input_sp' in YAML file '$YAML_file'\n";
+
+#Read YAML
+my $data = LoadFile($YAML_file);
+
+#Search
+my $found = 0;
+my $output = ' ';
+
+for my $k1 (keys %{$data}) {
+    for my $k2 (keys %{$data->{$k1}}) {
+        #print "Key:    $k2 => @{$data->{$k1}{$k2}}\n";
+        #print "$k2\n";
+        #print "@{$data->{$k1}{$k2}} \n";
+        # transform arrays into strings
+        my $string1 = join('-' ,$k2);
+        my $string2 = join( '-', @{$data->{$k1}{$k2}} );
+        print "Key:  '$string1' -> '$string2' \n";
+        #print "Input $input_sp \n";
+
+        if ($string1 eq $input_sp){
+          print "MATCH ! \n";
+          $found +=1;
+          $output = $string2 ;
+        }
+    }
+}
+
+print " Number of occurences: $found \n ";
+
+if ($found eq 0){
+  print " $input_sp \n ";
+} else {
+  print "$output \n ";
+}
+
+
