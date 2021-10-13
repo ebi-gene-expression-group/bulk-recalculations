4029d9dfe5b509821da4b2d7a7c5b1ad02d4aa99
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-10-13 12:59:17 +0100

    Add new logic in log handler to retrieve precise information of which rule/s are failing

--- log_handler.py
+++ log_handler.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import os
+import re
 
 l = logging.getLogger(__name__)
 l.setLevel(logging.DEBUG)
@@ -33,7 +34,50 @@ def log_handler(msg):
                 log=""
                 if 'log' in j_cont:
                     log=j_cont['log']
-                l.info(f"{j_cont['accession']}\t{j_cont['name']}\t{j_cont['status']}\t{log}")
+
+                wdir = os.getcwd()
+                log_path = wdir+"/"+log
+                errorInfo = "Status: "
+                tail_e="(check log file(s) for error message)"
+
+                if j_cont['status']== "job_finished":
+                    errorInfo+="OK"
+                else:
+                    errorInfo+="ERROR"
+                #report log
+                l.info(f"{j_cont['accession']}\t{j_cont['name']}\t{j_cont['status']}\t{log_path}\t {errorInfo}" )
+
+                if j_cont['status']== "job_error":
+                    if j_cont['name']== "get_experiment_metadata":
+                        if os.path.isfile(log_path):
+                            file = open( log_path, "r")
+                            err=[]
+                            for line in file:                                                                                                                                                                            
+                                if re.search("error", line, re.IGNORECASE):                                                                                                                                              
+                                    err.append(line.strip())                                                                                                                                                               
+                            if len(err)>0:                                                                                                                                                                                 
+                                l.info(f"-- error/s found get_experiment_metadata: {len(err)}" )                                                                                                                           
+                                for i in range(len(err)):                                                                                                                                                                  
+                                    l.info(f"---{i}\t{j_cont['accession']}\t{err[i]}" )                                                                                                                                    
+                                                                                                                                                                                                                         
+                    if j_cont['name']== "produce_recalculations_call":                                                                                                                                                   
+                        if os.path.isfile(log_path):                                                                                                                                                                     
+                            file = open( log_path, "r")                                                                                                                                                                  
+                            err0=[]                                                                                                                                                                                         
+                            err1=[]                                                                                                                                                                                         
+                            for line in file:                                                                                                                                                                            
+                                if re.search("error in rule", line, re.IGNORECASE):                                                                                                                                                                                                                                                                                        
+                                    err0.append(line.strip())                                                                                                                                                               
+                                if re.search("for error message\)", line, re.IGNORECASE):                                                                                                                                                                                                                                                                                 
+                                    err1.append(line.strip())                                                                                                                                                               
+                            if len(err0)>0:                                                                                                                                                                                 
+                                l.info(f"-- error in rules: {len(err0)}" )                                                                                                                                                  
+                                l.info(f"-- error messages: {len(err1)}" )                                                                                                                                                  
+                                l.info(f"-- restart-times: {(len(err1)/len(set(err1)))-1 } ")                                                                                                                                  
+                                for i in range(len(err0)):                                                                                                                                                                  
+                                    err=err1[i].replace(tail_e,"")                                                                                                                                                            
+                                    l.info(f"---{i}\t{j_cont['accession']}\t{err0[i]}\t{err} ")                                                                                                                               
+                                                                                                                                                                                                                                                      
                 jobs_to_remove.append(j_id)
                 #sys.stdout.flush()
 
