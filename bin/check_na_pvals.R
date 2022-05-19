978940fddd8f3350c47a4a819d25fc3cb2ee9acc
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-05-19 16:04:34 +0100

    Update bin/check_na_pvals.R
    Co-authored-by: Pablo Moreno <pcm32@users.noreply.github.com>

--- bin/check_na_pvals.R
+++ bin/check_na_pvals.R
@@ -1,15 +1,10 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs( TRUE )
-
 analyticsFilename <- args[ 1 ]
-
 analyticsResults <- read.delim( analyticsFilename, header=TRUE )
-
 pvalueColnames <- colnames( analyticsResults )[ grep( "p.value", colnames( analyticsResults ) ) ]
-
 nonNAcounts <- sapply( pvalueColnames, function( x ) {
-    
     length( which( !is.na( analyticsResults[[ x ]] ) ) )
 } )
 
