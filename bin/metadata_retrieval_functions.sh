e6316d37469243848ba5b39bc4c5ca860183ecba
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2022-05-26 15:27:50 +0100

    delete metadata_retrieval_functions.sh (refactored)

--- bin/metadata_retrieval_functions.sh
+++
@@ -1,6 +0,0 @@
-#This is the fastest but is only possible if the condensed sdrf has been already written out
-#We only condense the sdrf after successful analysis, and analysis needs organism name in e.g. GSEA, so we can't always use this
-#This assumes the organism field there is unique (it fetches the first one that appears otherwise)
-get_organism_from_condensed_sdrf(){
-    cut -f 4,5,6 $1 | grep characteristic$'\t'organism$'\t' | head -n1 | cut -f 3 | to_ensembl_species_lowercase
-}
