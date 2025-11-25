5d05f6f19ea1b97e1d0d9a4fb016ca867220474a
Anil Thanki <thanki.anil@gmail.com>
2025-11-25 12:04:44 +0000

    Add script description comment
    Add a comment to describe the script's functionality.

---
+++ bin/gxa_generate_method.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Generate a simple tsv formatted table from pipeline_info/nf_core_rnaseq_software_mqc_versions.yml of nf-core/rnaseq workflow
+
+import yaml
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write("Usage: yaml_to_tsv.py nf_core_rnaseq_software_mqc_versions.yml\n")
+    sys.exit(1)
+
+with open(sys.argv[1]) as f:
+    data = yaml.safe_load(f)
+
+# Output header
+print("module\ttool\tversion")
+
+for module, tools in data.items():
+    # Skip workflow block
+    if module == "Workflow":
+        continue
+
+    # tools may be dict or other
+    if isinstance(tools, dict):
+        for tool, ver in tools.items():
+            print(f"{module}\t{tool}\t{ver}")
